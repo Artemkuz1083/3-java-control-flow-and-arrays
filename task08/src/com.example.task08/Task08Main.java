33cf3ce60713828cecdb0ffcf76e8789f026d2f3
Artem Kuznetsov <artemkuz1083@gmail.com>
2024-09-20 00:44:32 +0500

    Кузнецов Артём. Задание 08

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -1,6 +1,6 @@
 package com.example.task08;
 
-public class Task08Main {
+public class    Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
@@ -13,8 +13,14 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
+        long a = 1;
+        if (arr.length == 0) return 0;
+        for(int i = 0; i<arr.length;i++){
+            a *= arr[i];
+        }
+
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        return a;
     }
 
 }
